5ee7ab5293280da488e5f1f5cc4c698a8d02c425
retecata <catalinarete@gmail.com>
2018-11-10 12:34:43 +0000

    Parsing

--- parseSentiment.py
+++ parseSentiment.py
@@ -2,33 +2,22 @@ import csv
 import re
 
 list = []
-unwanted = {'WEBSITE2018','@USER'}
-
-
+unwanted = {'WEBSITE2018','@USER','&quot'}
 emoji_pattern = re.compile("["
         u"\U0001F600-\U0001F64F"  # emoticons
         u"\U0001F300-\U0001F5FF"  # symbols & pictographs
         u"\U0001F680-\U0001F6FF"  # transport & map symbols
         u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-        u"\U00010000-\U0010ffff"
+        u"WEBSITE2018"
+        u"@USER"
                            "]+", flags=re.UNICODE)
 
-with open('dataset1_interim_eval.csv',encoding='utf8',errors = 'ignore') as csv_file:
+with open('dataset2.csv',encoding='utf8',errors = 'ignore') as csv_file:
     csv_reader = csv.reader(csv_file,delimiter=',')
     line_count = 0
     for row in csv_reader:
-        row[1] = row[1].split()
-
-        # removing @USER and hashtags
-        row[1] = [e for e in row[1] if not (e.startswith('#') or e in unwanted)]
-        text = row[1]
-        i=0;
-        new_row = ''
-        while i < len(text):
-            text[i]  = emoji_pattern.sub(r'',text[i])
-            new_row = new_row +' ' +text[i]
-            i +=1
-
+        row[1] = emoji_pattern.sub(r'',row[1])
+        new_row = row[1].replace('&quot','')
         row[1] = new_row
         list.append(row)
 
