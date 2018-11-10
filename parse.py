5610c60ebb0069ca71551b80ae42d4e84817a8ba
retecata <catalinarete@gmail.com>
2018-11-10 10:12:37 +0000

    Removing WEBSITE2018 and @USER

--- parse.py
+++ parse.py
@@ -2,12 +2,18 @@ import csv
 
 
 list = []
+unwanted = {'WEBSITE2018','@USER'}
 with open('dataset1.csv') as csv_file:
     csv_reader = csv.reader(csv_file,delimiter=',')
     line_count = 0
     for row in csv_reader:
+        text = row[1]
+        list_text = text.split()
+        row[1] = [e for e in list_text if e not in unwanted]
         list.append(row)
 
 
-// Index 0 represents the columns names.
-print(list[0])
+
+# Index 0 represents the columns names.
+print(list[1])
+print(list[2])
